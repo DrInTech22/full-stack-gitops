567df87b623d2357c5659e97c37a99d3240b0cbd
Okesanya Odunayo <94924061+DrInTech22@users.noreply.github.com>
2024-12-13 10:47:48 +0100

    modify ec2.tf, playbook

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -13,3 +13,13 @@ resource "aws_instance" "ec2" {
   }
 }
 
+resource "aws_instance" "ec2-2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_pair_name
+  subnet_id     = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = var.ec2_name
+  }
+}
